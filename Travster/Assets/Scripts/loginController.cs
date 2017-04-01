dbc0bacdd5de4bbbbf6697607b4701084a8a7b77
sekaranglila <sekaranglilah@gmail.com>
2017-04-01 15:31:09 +0700

    Login Page Added

---
+++ Travster/Assets/Scripts/loginController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loginController : MonoBehaviour {
+
+	public InputField email;
+	public InputField pass;
+	public static string em;
+	public static string p;
+
+	void Awake(){
+		email = GameObject.Find ("Email").GetComponent<InputField> ();
+		pass = GameObject.Find ("Password").GetComponent<InputField> ();
+	}
+
+	public void submit(){
+		em = email.text;
+		email.text = "";
+
+		p = pass.text;
+		pass.text = "";
+
+		Debug.Log (em);
+		Debug.Log (p);
+	}
+}
